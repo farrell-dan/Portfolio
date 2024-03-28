4228905d954c4106f19d41c58c20253c62a8330f
Daniel Farrell <farrelld94@live.com>
2024-03-27 21:29:26 -0400

    About Me Update

--- client/src/components/Main Page/AboutMe.js
+++ client/src/components/Main Page/AboutMe.js
@@ -10,26 +10,25 @@ const AboutMe = () => {
 				<Column>
 					<h2>Biography</h2>
 					<p>
-						I am a dedicated professional currently transitioning to a career in
-						Full Stack Web Development. With a solid background in the health
-						and wellness field, I have honed my adaptability skills in diverse
-						environments. Coming from a life of team sports I excel while
-						working in a collaborative setting. After successfully completing my
-						diploma in Web Development, I am now well-equipped and enthusiastic
-						to apply my skills and embark on a fulfilling journey in this
-						dynamic field. Eager to leverage my expertise, I am committed to
-						creating innovative and user-centric solutions in the ever-evolving
-						digital landscape.
+						My name is Daniel Farrell, I'm currently going through a career
+						change into Full Stack Web Development. Being a Certified Athletic
+						Therapist I have a solid background in the health and wellness
+						field. I am used to working in fast past, high stress, dynamic
+						environments and having being evolved in team sports my entire life
+						I excel while working in a collaborative setting. Having
+						successfully completing my diploma in Web Development at Concordia
+						University, I am now well-equipped and looking to enter the tech
+						field.
 					</p>
 					<p>
-						I am a certified athletic therapist who is very passionate about
-						health and wellness. I am a quick learner who adapts easily in new
-						environments. Having played team sports my entire life, I thrive
-						while working as part of a team. I have enjoyed my experiences
-						working in clinics, gyms and on the field as an athletic therapist
-						and I am now looking for a new challenge.
+						I have enjoyed my experiences working in clinics, gyms and on the
+						field as an athletic therapist. I enjoy staying active and moving.
+						Playing soccer, snowboarding, biking and running are some of my go
+						to activities and when I need a rest I enjoy working on puzzles. And
+						now in my free time I am trying to learn as much as I can in the web
+						development field.
 					</p>
-					<p>
+					{/* <p>
 						I believe I am the best candidate for the web developer position due
 						to a combination of my strong work ethic, passion for the field, and
 						unique skill set acquired through my experience as an athletic
@@ -53,7 +52,7 @@ const AboutMe = () => {
 						handle stress, and dedication to ongoing learning make me the ideal
 						candidate to contribute effectively to the collaborative and
 						innovative environment at your company.
-					</p>
+					</p> */}
 				</Column>
 				<RightColumn>
 					<h2>My CV</h2>
